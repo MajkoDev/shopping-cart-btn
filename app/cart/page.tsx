3d209f7578a57fa6d43856c20824b6d49e5d1b09
MajkoDev <hellomariandev@gmail.com>
2024-03-04 23:43:06 +0100

    finished

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 export default function Page(){
   return(
-    <div className='my-24'>
-      <h1 className='text-center font-extralight text-3xl'>Shopping Cart</h1>
+    <div>
+      <div className="flex w-full flex-row justify-center items-center">
+        <Link href='/' className="bg-bold text-center hover:underline">Back Home</Link>
+      </div>
+      <h1 className='text-center font-extralight text-3xl my-24'>Shopping Cart</h1>
     </div>
   )
 }
